b49337830ee4384947477c0f95c9262a43a8b81f
mehulbots <141015568+mehulbots@users.noreply.github.com>
2023-07-31 18:41:53 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,47 @@
-# (c) @AbirHasan2005
+# # (c) @AbirHasan2005
+
+# from bot.get_cfg import get_config
+
+# class Config(object):
+#     # You can keep this default
+#     SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
+#     # Put MongoDB URL
+#     DATABASE_URL = get_config("DATABASE_URL", "mongodb+srv://Mehul:Mehul@cluster0.1ihhsfr.mongodb.net/")
+#     # get a token from @BotFather
+#     TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "5994576721:AAE_6iKcCEnbCCnT4u9AezKOMxuVtW_MZgI")
+#     # The Telegram API things
+#     APP_ID = int(get_config("APP_ID", 23322236)) 
+#     API_HASH = get_config("API_HASH", "e7cc5d762451079c046202fc1d926677")
+#     LOG_CHANNEL = get_config("LOG_CHANNEL")
+#     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", "mehulbots") # Without `@` LOL
+#      # Get these values from my.telegram.org
+#     # array to store the channel ID who are authorized to use the bot
+#     AUTH_USERS = set(
+#         int(x) for x in get_config(
+#             "AUTH_USERS",
+#             should_prompt=True
+#         ).split()
+#     )
+#     # the download location, where the HTTP Server runs
+#     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
+#     # Telegram maximum file upload size
+#     BOT_USERNAME = get_config("BOT_USERNAME", "")
+#     MAX_FILE_SIZE = 2097152000
+#     TG_MAX_FILE_SIZE = 2097152000
+#     FREE_USER_MAX_FILE_SIZE = 2097152000
+#     # default thumbnail to be used in the videos
+#     DEF_THUMB_NAIL_VID_S = get_config("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+#     # proxy for accessing youtube-dl in GeoRestricted Areas
+#     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+#     HTTP_PROXY = get_config("HTTP_PROXY", None)
+#     # maximum message length in Telegram
+#     MAX_MESSAGE_LENGTH = 4096
+#     # add config vars for the display progress
+#     FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "▓")
+#     UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "░")
+#     LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
+#       # because, https://t.me/c/1494623325/5603
+#     SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
 
 from bot.get_cfg import get_config
 
@@ -10,18 +53,19 @@ class Config(object):
     # get a token from @BotFather
     TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "5994576721:AAE_6iKcCEnbCCnT4u9AezKOMxuVtW_MZgI")
     # The Telegram API things
-    APP_ID = int(get_config("APP_ID", 23322236)) 
-    API_HASH = get_config("API_HASH", "e7cc5d762451079c046202fc1d926677")
+    APP_ID = int(get_config("APP_ID", 28888037)) 
+    API_HASH = get_config("API_HASH", "9fbe164b5591df05fbd8577e3b1d6d21")
     LOG_CHANNEL = get_config("LOG_CHANNEL")
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", "mehulbots") # Without `@` LOL
-     # Get these values from my.telegram.org
+    # Get these values from my.telegram.org
     # array to store the channel ID who are authorized to use the bot
     AUTH_USERS = set(
         int(x) for x in get_config(
             "AUTH_USERS",
+            default="",
             should_prompt=True
         ).split()
-    )
+    ) if get_config("AUTH_USERS") else set()
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
@@ -40,5 +84,6 @@ class Config(object):
     FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "▓")
     UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "░")
     LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
-      # because, https://t.me/c/1494623325/5603
+    # because, https://t.me/c/1494623325/5603
     SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
+
