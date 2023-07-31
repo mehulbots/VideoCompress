34707a23686a6f2e68ce73dfd30c0d07fe40f2d7
mehulbots <141015568+mehulbots@users.noreply.github.com>
2023-07-31 17:22:22 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,14 +6,14 @@ class Config(object):
     # You can keep this default
     SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
     # Put MongoDB URL
-    DATABASE_URL = get_config("DATABASE_URL", "")
+    DATABASE_URL = get_config("DATABASE_URL", "mongodb+srv://Mehul:Mehul@cluster0.1ihhsfr.mongodb.net/")
     # get a token from @BotFather
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "5994576721:AAE_6iKcCEnbCCnT4u9AezKOMxuVtW_MZgI")
     # The Telegram API things
-    APP_ID = int(get_config("APP_ID", 12345))
-    API_HASH = get_config("API_HASH", "")
+    APP_ID = int(get_config("APP_ID", 28888037))
+    API_HASH = get_config("API_HASH", "9fbe164b5591df05fbd8577e3b1d6d21")
     LOG_CHANNEL = get_config("LOG_CHANNEL")
-    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
+    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", "mehulbots") # Without `@` LOL
      # Get these values from my.telegram.org
     # array to store the channel ID who are authorized to use the bot
     AUTH_USERS = set(
